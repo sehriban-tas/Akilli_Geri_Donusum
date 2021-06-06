d7e42bd61d6fdbf420daa3f3ca7d4d996ccc360e
sevda engn <svdngn@outlook.com>
2021-06-06 14:37:27 +0300

    new commit

--- index.php
+++ index.php
@@ -300,34 +300,7 @@ require("admin/layouts/db.php");
                     </div>
                 </div>
             </div>
-            <div class="col-md-8 col-md-offset-2">
-                <h3>Leave us a message</h3>
-                <form name="sentMessage" id="contactForm" novalidate>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="text" id="name" class="form-control" placeholder="Name"
-                                    required="required">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input type="email" id="email" class="form-control" placeholder="Email"
-                                    required="required">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message"
-                            required></textarea>
-                        <p class="help-block text-danger"></p>
-                    </div>
-                    <div id="success"></div>
-                    <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
-                </form>
-            </div>
+          
         </div>
     </div>
     <!-- Footer Section -->
